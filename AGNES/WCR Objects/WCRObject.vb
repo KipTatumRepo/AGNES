43e76b2aafe1904cb68c40d8b7333f83b67f3b57
Brian Freeman <v-brif@microsoft.com>
2018-06-12 06:32:12 -0700

    Tender extension

--- AGNES/WCR Objects/WCRObject.vb
+++ AGNES/WCR Objects/WCRObject.vb
@@ -1,36 +1,65 @@
 ﻿Imports Microsoft.Win32
 Imports Microsoft.Office.Interop
 Public Class WCRObject
+    Public Tenders As New List(Of TenderObject)
     Public Sub New()
-        Dim Tender As New TenderObject
+        Dim ph As String = ""
     End Sub
-    Public Sub LoadTenders()
+    Public Sub LoadTenders(ByRef disp As WCRHello)
         Dim fd As New OpenFileDialog()
         fd.DefaultExt = ".xls"
         fd.Filter = "Excel (97-2003) documents (.xls)|*.xls"
-
-        ' Display OpenFileDialog by calling ShowDialog method
         Dim result As Nullable(Of Boolean) = fd.ShowDialog()
-
-        ' Get the selected file name and display in a TextBox
         If result = True Then
-            ' Open document
+            Dim ti As New TenderObject
+            Tenders.Add(ti)
             Dim filename As String = fd.FileName
             Dim xlApp As New Excel.Application(), wb As Excel.Workbook = xlApp.Workbooks.Open(filename)
-            Dim ws As Excel.Worksheet = wb.Sheets(1), valz As String = "", tender As String = "", qty As Integer = 0, ttl As Double = 0, ct As Integer = 1
-            Do Until valz = "Tenders"
-                valz = CType(ws.Cells(ct, 2), Excel.Range).Value
+            Dim ws As Excel.Worksheet = wb.Sheets(1), valz As String = "", tendert As String = "", qty As Integer = 0, ttl As Double = 0, ct As Integer = 1
+            Do Until Left(valz, 13) = "Selected For:"
+                valz = CType(ws.Cells(ct, 1), Excel.Range).Value
                 ct += 1
             Loop
+            ti.VendorName = GetVendorNameFromString(valz)
+            ct += 3
             Do Until valz = "Subtotal"
-                tender = CType(ws.Cells(ct, 2), Excel.Range).Value
+                tendert = CType(ws.Cells(ct, 2), Excel.Range).Value
                 qty = FormatNumber(CType(ws.Cells(ct, 3), Excel.Range).Value, 0)
                 ttl = FormatCurrency(CType(ws.Cells(ct, 9), Excel.Range).Value, 2)
-                MsgBox(tender & " has a quantity of " & qty & " and a net tender of " & ttl & ".")
+                ti.AddItem(tendert, qty, ttl)
                 ct += 1
                 valz = CType(ws.Cells(ct, 1), Excel.Range).Value
             Loop
+
+            wb.Close()
+            xlApp.Quit()
+            releaseObject(ws)
+            releaseObject(wb)
+            releaseObject(xlApp)
+            disp.PrintToScreen(ti)
         End If
 
+    End Sub
+    Private Function GetVendorNameFromString(st)
+        Dim vn As String = st
+        Dim si As Integer = vn.IndexOf("(")
+        Dim li As Integer = vn.IndexOf(")")
+        Dim vnum As Integer = FormatNumber(vn.Substring(si + 1, (li - si) - 1))
+        'TODO: Replace hard code with db table lookup for store num -> vendor name
+        Select Case vnum
+            Case 499
+                vn = "Typhoon"
+            Case Else
+                vn = "Nada"
+        End Select
+        Return vn
+    End Function
+    Private Sub releaseObject(ByVal obj As Object)
+        Try
+            System.Runtime.InteropServices.Marshal.ReleaseComObject(obj)
+            obj = Nothing
+        Catch ex As Exception
+            obj = Nothing
+        End Try
     End Sub
 End Class
--- AGNES/WCRHello.xaml.vb
+++ AGNES/WCRHello.xaml.vb
@@ -1,5 +1,7 @@
 ﻿Public Class WCRHello
-    Shared WCR As New WCRObject
+    Private Property _currentstate As Integer
+
+    Public Shared WCR As New WCRObject
     Private Sub WCRHello_Loaded(sender As Object, e As RoutedEventArgs) Handles Me.Loaded
         tbHello.Text = "Hi, " & MySettings.Default.UserName & "!  It's me, Agnes, and I'll be guiding you through your WCR entry today." & Chr(13) & Chr(13) & "Let's get started by choosing whether we want to look at a past WCR or create a new one!"
     End Sub
@@ -9,10 +11,44 @@
         btnCreateWCR.Visibility = Visibility.Hidden
         btnViewWCR.Visibility = Visibility.Hidden
         btnLoadTenders.Visibility = Visibility.Visible
-        tbHello.Text = "Sounds good, " & MySettings.Default.UserName & ".  First things first, choose your Sales Tender Summary file and I'll pull in the information."
+        Dim wkst As Date = Now().Date : wkst = wkst.AddDays(-1)
+        Do Until Weekday(wkst, FirstDayOfWeek.Friday) = 1
+            wkst = wkst.AddDays(-1)
+        Loop
+
+        tbHello.Text = "Sounds good, " & MySettings.Default.UserName & ".  Let's get started on the WCR for the week starting " & wkst & ".  First things first, choose your Sales Tender Summary file and I'll pull in the information."
     End Sub
 
-    Private Sub btnLoadTenders_Click(sender As Object, e As RoutedEventArgs) Handles btnLoadTenders.Click
-        WCR.LoadTenders()
+    Private Sub LoadTenderFile(sender As Object, e As RoutedEventArgs) Handles btnLoadTenders.Click, btnAnother.Click
+        WCR.LoadTenders(Me)
+    End Sub
+    Private Sub LoadAnotherTenderFile(sender As Object, e As RoutedEventArgs) Handles btnYes.Click
+        btnYes.Visibility = Visibility.Hidden
+        btnNo.Visibility = Visibility.Hidden
+        btnAnother.Visibility = Visibility.Visible
+        btnDone.Visibility = Visibility.Visible
+    End Sub
+
+    Public Sub PrintToScreen(ByRef t As TenderObject)
+        Dim amt As Double, ttl As Double
+        tbHello.FontSize = 12
+        For Each amt In t.TenderAmt
+            ttl += amt
+        Next
+        tbHello.Text = "It looks like " & t.VendorName & " has a total of " & FormatCurrency(ttl, 2) & ".  Is this correct?"
+        btnLoadTenders.Visibility = Visibility.Hidden
+        btnYes.Visibility = Visibility.Visible
+        btnNo.Visibility = Visibility.Visible
+    End Sub
+
+    Private Sub btnDone_Click(sender As Object, e As RoutedEventArgs) Handles btnDone.Click
+        tbHello.FontSize = 30
+        tbHello.Text = "Great!  Let's move on to CAM checks..."
+        btnLoadTenders.Visibility = Visibility.Hidden
+        btnYes.Visibility = Visibility.Hidden
+        btnNo.Visibility = Visibility.Hidden
+        btnAnother.Visibility = Visibility.Hidden
+        btnDone.Visibility = Visibility.Hidden
+
     End Sub
 End Class
