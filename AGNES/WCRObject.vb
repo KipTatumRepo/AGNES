48c2c0b1c19203e16f4ecdb5268582c528546d97
Brian Freeman <v-brif@microsoft.com>
2018-06-11 18:48:52 -0700

    Reading tender file

--- AGNES/WCRObject.vb
+++ AGNES/WCRObject.vb
@@ -1,4 +1,5 @@
 ﻿Imports Microsoft.Win32
+Imports Microsoft.Office.Interop
 Public Class WCRObject
     Public Sub New()
         Dim Tender As New TenderObject
@@ -15,7 +16,20 @@ Public Class WCRObject
         If result = True Then
             ' Open document
             Dim filename As String = fd.FileName
-            MsgBox(filename)
+            Dim xlApp As New Excel.Application(), wb As Excel.Workbook = xlApp.Workbooks.Open(filename)
+            Dim ws As Excel.Worksheet = wb.Sheets(1), valz As String = "", tender As String = "", qty As Integer = 0, ttl As Double = 0, ct As Integer = 1
+            Do Until valz = "Tenders"
+                valz = CType(ws.Cells(ct, 2), Excel.Range).Value
+                ct += 1
+            Loop
+            Do Until valz = "Subtotal"
+                tender = CType(ws.Cells(ct, 2), Excel.Range).Value
+                qty = FormatNumber(CType(ws.Cells(ct, 3), Excel.Range).Value, 0)
+                ttl = FormatCurrency(CType(ws.Cells(ct, 9), Excel.Range).Value, 2)
+                MsgBox(tender & " has a quantity of " & qty & " and a net tender of " & ttl & ".")
+                ct += 1
+                valz = CType(ws.Cells(ct, 1), Excel.Range).Value
+            Loop
         End If
 
     End Sub
