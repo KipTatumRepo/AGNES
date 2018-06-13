5b1ee5f6636c368a392815ce00562cb5b669a5f9
Brian Freeman <v-brif@microsoft.com>
2018-06-12 20:10:40 -0700

    Cam screen started, with animations

--- AGNES/MainWindow.xaml.vb
+++ AGNES/MainWindow.xaml.vb
@@ -1,12 +1,17 @@
-﻿Class MainWindow
+﻿Public Class MainWindow
+    Public Shared WCR As New WCRObject
     Public Sub New()
         InitializeComponent()
         MySettings.Default.UserName = Environment.UserName
+
     End Sub
 
     Private Sub Button_Click(sender As Object, e As RoutedEventArgs)
         Dim WCRStartPage As New WCRHello
         Hide()
         WCRStartPage.ShowDialog()
+        Dim WCRCamPage As New WCRCam
+        WCRCamPage.ShowDialog()
+
     End Sub
 End Class
--- AGNES/WCRHello.xaml.vb
+++ AGNES/WCRHello.xaml.vb
@@ -1,7 +1,7 @@
 ﻿Public Class WCRHello
     Private Property _currentstate As Integer
 
-    Public Shared WCR As New WCRObject
+
     Private Sub WCRHello_Loaded(sender As Object, e As RoutedEventArgs) Handles Me.Loaded
         tbHello.Text = "Hi, " & MySettings.Default.UserName & "!  It's me, Agnes, and I'll be guiding you through your WCR entry today." & Chr(13) & Chr(13) & "Let's get started by choosing whether we want to look at a past WCR or create a new one!"
     End Sub
@@ -16,11 +16,11 @@
             wkst = wkst.AddDays(-1)
         Loop
 
-        tbHello.Text = "Sounds good, " & MySettings.Default.UserName & ".  Let's get started on the WCR for the week starting " & wkst & ".  First things first, choose your Sales Tender Summary file and I'll pull in the information."
+        tbHello.Text = "Sounds good, " & MySettings.Default.UserName & ".  Let's get started on the WCR for the week starting " & wkst & "." & Chr(13) & Chr(13) & "First things first, choose your Sales Tender Summary file and I'll pull in the information."
     End Sub
 
     Private Sub LoadTenderFile(sender As Object, e As RoutedEventArgs) Handles btnLoadTenders.Click, btnAnother.Click
-        WCR.LoadTenders(Me)
+        MainWindow.WCR.LoadTenders(Me)
     End Sub
     Private Sub LoadAnotherTenderFile(sender As Object, e As RoutedEventArgs) Handles btnYes.Click
         btnYes.Visibility = Visibility.Hidden
@@ -43,12 +43,11 @@
 
     Private Sub btnDone_Click(sender As Object, e As RoutedEventArgs) Handles btnDone.Click
         tbHello.FontSize = 30
-        tbHello.Text = "Great!  Let's move on to CAM checks..."
         btnLoadTenders.Visibility = Visibility.Hidden
         btnYes.Visibility = Visibility.Hidden
         btnNo.Visibility = Visibility.Hidden
         btnAnother.Visibility = Visibility.Hidden
         btnDone.Visibility = Visibility.Hidden
-
+        Close()
     End Sub
 End Class
