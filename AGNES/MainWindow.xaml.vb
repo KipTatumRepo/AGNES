8d01612c60095ac576a51cb3870fb6ac647846b8
Brian Freeman <v-brif@microsoft.com>
2018-06-14 13:58:01 -0700

    Not much

--- AGNES/MainWindow.xaml.vb
+++ AGNES/MainWindow.xaml.vb
@@ -1,15 +1,16 @@
 ﻿Public Class MainWindow
     Public Shared WCR As New WCRObject
+
     Public Sub New()
         InitializeComponent()
         MySettings.Default.UserName = Environment.UserName
     End Sub
 
-    Private Sub Button_Click(sender As Object, e As RoutedEventArgs)
+    Public Sub Button_Click(sender As Object, e As RoutedEventArgs)
+        Close()
         Dim WCRStartPage As New WCRHello
         Dim WCRCamPage As New WCRCam
         Dim WCRFinalPage As New WCRFinal
-        Hide()
         WCRStartPage.ShowDialog()
         WCRCamPage.ShowDialog()
         WCRFinalPage.ShowDialog()
