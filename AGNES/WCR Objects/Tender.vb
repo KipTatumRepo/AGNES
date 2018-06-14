8c9cbc03354993132dc406a0660dacb9996984fe
Brian Freeman <v-brif@microsoft.com>
2018-06-13 18:19:17 -0700

    Starting GL assignment in Tender class

--- AGNES/WCR Objects/Tender.vb
+++ AGNES/WCR Objects/Tender.vb
@@ -1,6 +1,22 @@
 ﻿Public Class Tender
+    Private _tenderid As Integer
     Public Property TenderID As Integer
+        Get
+            Return _tenderid
+        End Get
+        Set(value As Integer)
+            GetGLCode(value)
+            _tenderid = value
+        End Set
+    End Property
     Public Property TenderName As String
     Public Property TenderQty As Integer
     Public Property TenderAmt As Double
+    Private Property GL As Long
+
+    Private Sub GetGLCode(tid)
+        'TODO: Connect to mapping table and look up associated GL
+        GL = 999
+    End Sub
+
 End Class
