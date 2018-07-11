83019e91564c39c8a21f28e760e444a5deb31b4c
Brian Freeman <v-brif@microsoft.com>
2018-07-11 13:39:05 -0700

    Further UI modifications

--- AGNES/BGCRM.xaml.vb
+++ AGNES/BGCRM.xaml.vb
@@ -1,7 +1,8 @@
 ﻿Public Class BGCRM
-    Private Sub SaveAndNext(sender As Object, e As RoutedEventArgs) Handles btnSaveNextGroup.Click
-        tabPages.SelectedIndex = 1
+    Private Sub SaveAndNext(sender As Object, e As RoutedEventArgs) Handles btnSaveNextGroup.Click, btnSaveNextPeople.Click, btnSaveFinish.Click, btnSaveNextEvents.Click, btnSaveNextFinances.Click
+        tabPages.SelectedIndex += 1
 
 
     End Sub
+
 End Class
