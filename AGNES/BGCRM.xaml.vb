921630219eda55856f3c75bed4b41dd6a3db893f
Brian Freeman <v-brif@microsoft.com>
2018-07-15 15:11:39 -0700

    Continuing to build option population routine

--- AGNES/BGCRM.xaml.vb
+++ AGNES/BGCRM.xaml.vb
@@ -50,5 +50,30 @@
         lbxLocationsSelect.Items.Clear()
         Dim loq = From bloc In BGC.Locations Select bloc Order By bloc.BuildingName
         For Each bloc In loq : lbxLocationsSelect.Items.Add(bloc.BuildingName) : Next
+
+        '// Populate leader options
+        cboLeader.Items.Clear()
+        Dim lq = From bldr In BGC.Leaders Select bldr Order By bldr.LeaderName
+        For Each bldr In lq : cboLeader.Items.Add(bldr.LeaderName) : Next
+
+        '// Populate relationship manager options - shares datasource with frequent customers
+        cboRelManager.Items.Clear()
+        Dim rmq = From brlm In BGC.FrequentCustomers Select brlm Order By brlm.CustomerName
+        For Each brlm In rmq : cboRelManager.Items.Add(brlm.CustomerName) : Next
+
+        '// Populate leadership team options
+        lbxLeadersSelect.Items.Clear()
+        Dim lsq = From blds In BGC.Leaders Select blds Order By blds.LeaderName
+        For Each blds In lsq : lbxLeadersSelect.Items.Add(blds.LeaderName) : Next
+
+        '// Populate frequent customers options
+        lbxCustomerSelect.Items.Clear()
+        Dim fcq = From bfc In BGC.FrequentCustomers Select bfc Order By bfc.CustomerName
+        For Each bfc In fcq : lbxCustomerSelect.Items.Add(bfc.CustomerName) : Next
+
+        '// Populate offsite location options
+        lbxOffsiteLocsSelect.Items.Clear()
+        Dim olq = From osl In BGC.OffsiteLocations Select osl Order By osl.OffsiteLocName
+        For Each osl In olq : lbxCustomerSelect.Items.Add(osl.OffsiteLocName) : Next
     End Sub
 End Class
