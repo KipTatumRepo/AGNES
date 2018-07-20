5d11d53a84cef0c95f3957e4e8e8e68a756036dc
Brian Freeman <v-brif@microsoft.com>
2018-07-20 10:45:26 -0700

    Functioning and kinda cool.  Base design established and close functionality implemented

---
+++ AGNES/RadialPortal.xaml.vb
@@ -0,0 +1,30 @@
+﻿Public Class RadialPortal
+    Public Sub New()
+
+        ' This call is required by the designer.
+        InitializeComponent()
+
+        ' Add any initialization after the InitializeComponent() call.
+
+    End Sub
+
+    Private Sub WCRGo(sender As Object, e As MouseButtonEventArgs) Handles btnRadial1.PreviewMouseLeftButtonDown
+        WCRModule.Runmodule()
+    End Sub
+
+    Private Sub BGCRMGo(sender As Object, e As MouseButtonEventArgs) Handles btnRadial2.PreviewMouseLeftButtonDown
+        Dim bgcrm As New BGCRM
+        bgcrm.ShowDialog()
+    End Sub
+
+    Private Sub DragViaLeftMouse(sender As Object, e As MouseButtonEventArgs)
+        DragMove()
+    End Sub
+
+    Private Sub CloseAGNES(sender As Object, e As MouseButtonEventArgs)
+        Dim ask As MsgBoxResult = MsgBox("Close AGNES?", MsgBoxStyle.YesNo)
+        If ask = MsgBoxResult.Yes Then
+            Close()
+        End If
+    End Sub
+End Class
