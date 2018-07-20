211370445ae8d80ccca5d2bb7c75eb02018f4404
Brian Freeman <v-brif@microsoft.com>
2018-07-20 11:26:34 -0700

    Added button expander/contracter routines and tooltip testing

--- AGNES/My Project/Resources.Designer.vb
+++ AGNES/My Project/Resources.Designer.vb
@@ -163,6 +163,16 @@ Namespace My.Resources
         '''<summary>
         '''  Looks up a localized resource of type System.Drawing.Bitmap.
         '''</summary>
+        Friend ReadOnly Property RadialUI_Agnes_Image() As System.Drawing.Bitmap
+            Get
+                Dim obj As Object = ResourceManager.GetObject("RadialUI_Agnes_Image", resourceCulture)
+                Return CType(obj,System.Drawing.Bitmap)
+            End Get
+        End Property
+        
+        '''<summary>
+        '''  Looks up a localized resource of type System.Drawing.Bitmap.
+        '''</summary>
         Friend ReadOnly Property Small_Agnes_Image() As System.Drawing.Bitmap
             Get
                 Dim obj As Object = ResourceManager.GetObject("Small_Agnes_Image", resourceCulture)
--- AGNES/RadialPortal.xaml.vb
+++ AGNES/RadialPortal.xaml.vb
@@ -27,4 +27,22 @@
             Close()
         End If
     End Sub
+
+    Private Sub ButtonHover(sender As Object, e As MouseEventArgs) Handles btnRadial1.MouseEnter, btnRadial2.MouseEnter, btnRadial3.MouseEnter, btnRadial4.MouseEnter
+        Dim s As Image = sender
+        s.Height = 105
+        s.Width = 105
+        Dim l As Integer = s.Margin.Left - 15
+        Dim t As Integer = s.Margin.Top - 15
+        s.Margin = New Thickness(l, t, 0, 0)
+
+    End Sub
+    Private Sub ButtonLeave(sender As Object, e As MouseEventArgs) Handles btnRadial1.MouseLeave, btnRadial2.MouseLeave, btnRadial3.MouseLeave, btnRadial4.MouseLeave
+        Dim s As Image = sender
+        s.Height = 75
+        s.Width = 75
+        Dim l As Integer = s.Margin.Left + 15
+        Dim t As Integer = s.Margin.Top + 15
+        s.Margin = New Thickness(l, t, 0, 0)
+    End Sub
 End Class
