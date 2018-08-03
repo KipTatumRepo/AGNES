e12183107143459be739ecce17b906ad7f21f8c6
Brian Freeman <v-brif@microsoft.com>
2018-08-02 18:01:21 -0700

    All WCR buttons converted to textblocks with dropshadow

--- AGNES/WCRHello.xaml.vb
+++ AGNES/WCRHello.xaml.vb
@@ -1,18 +1,23 @@
 ﻿Public Class WCRHello
     Private Property _currentstate As Integer
     Private ActiveWCR As WCRObject = WCRModule.WCR
+    Dim HoverDrop As Effects.DropShadowEffect, LeaveDrop As Effects.DropShadowEffect
 
     Private Sub WCRHello_Loaded(sender As Object, e As RoutedEventArgs) Handles Me.Loaded
         'TODO: Map to user table to get short name, etc.
         ActiveWCR.Author = MySettings.Default.UserName
         ActiveWCR.ShortName = "pal"
         tbHello.Text = "Hi, " & ActiveWCR.ShortName & "!  It's me, Agnes, and I'll be guiding you through your WCR entry today." & Chr(13) & Chr(13) & "Let's get started by choosing whether we want to look at a past WCR or create a new one!"
+        tbViewWCR.Visibility = Visibility.Visible
+        tbCreateWCR.Visibility = Visibility.Visible
+        HoverDrop = New Effects.DropShadowEffect With {.Color = Color.FromRgb(235, 235, 235), .Direction = 200, .Opacity = 100, .ShadowDepth = 6, .BlurRadius = 2, .RenderingBias = Effects.RenderingBias.Performance}
+        LeaveDrop = New Effects.DropShadowEffect With {.Color = Color.FromRgb(235, 235, 235), .Direction = 200, .Opacity = 100, .ShadowDepth = 4, .BlurRadius = 2, .RenderingBias = Effects.RenderingBias.Performance}
     End Sub
 
-    Private Sub CreateNewWCR(sender As Object, e As RoutedEventArgs) Handles btnCreateWCR.Click
-        btnCreateWCR.Visibility = Visibility.Hidden
-        btnViewWCR.Visibility = Visibility.Hidden
-        btnLoadTenders.Visibility = Visibility.Visible
+    Private Sub CreateNewWCR(sender As Object, e As MouseEventArgs) Handles tbCreateWCR.MouseDown
+        tbCreateWCR.Visibility = Visibility.Hidden
+        tbViewWCR.Visibility = Visibility.Hidden
+        tbLoadTenders.Visibility = Visibility.Visible
         Dim wkst As Date = Now().Date : wkst = wkst.AddDays(-1)
         Do Until Weekday(wkst, FirstDayOfWeek.Friday) = 1
             wkst = wkst.AddDays(-1)
@@ -21,39 +26,40 @@
         ActiveWCR.WeekStart = wkst
     End Sub
 
-    Private Sub ViewSavedWCR(sender As Object, e As RoutedEventArgs) Handles btnViewWCR.Click
+    Private Sub ViewSavedWCR(sender As Object, e As MouseEventArgs) Handles tbCreateWCR.MouseDown
         Dim ph As String = ""
         'TODO: CREATE ENTIRE ROUTINE FOR LOADING SAVED WCR FILE
     End Sub
 
-    Private Sub LoadTenderFile(sender As Object, e As RoutedEventArgs) Handles btnLoadTenders.Click, btnAnother.Click
+    Private Sub LoadTenderFile(sender As Object, e As MouseEventArgs) Handles tbLoadTenders.MouseDown, tbAnother.MouseDown
         WCRModule.WCR.LoadTenders(Me)
     End Sub
 
-    Private Sub LoadAnotherTenderFile(sender As Object, e As RoutedEventArgs) Handles btnYes.Click
-        btnYes.Visibility = Visibility.Hidden
-        btnNo.Visibility = Visibility.Hidden
-        btnAnother.Visibility = Visibility.Visible
-        btnDone.Visibility = Visibility.Visible
+    Private Sub LoadAnotherTenderFile(sender As Object, e As MouseEventArgs) Handles tbYes.MouseDown
+        tbYes.Visibility = Visibility.Hidden
+        tbNo.Visibility = Visibility.Hidden
+        tbAnother.Visibility = Visibility.Visible
+        tbDone.Visibility = Visibility.Visible
     End Sub
 
-    Private Sub btnDone_Click(sender As Object, e As RoutedEventArgs) Handles btnDone.Click
+    Private Sub btnDone_Click(sender As Object, e As MouseEventArgs) Handles tbDone.MouseDown
         tbHello.FontSize = 30
-        btnLoadTenders.Visibility = Visibility.Hidden
-        btnYes.Visibility = Visibility.Hidden
-        btnNo.Visibility = Visibility.Hidden
-        btnAnother.Visibility = Visibility.Hidden
-        btnDone.Visibility = Visibility.Hidden
+        tbLoadTenders.Visibility = Visibility.Hidden
+        tbYes.Visibility = Visibility.Hidden
+        tbNo.Visibility = Visibility.Hidden
+        tbAnother.Visibility = Visibility.Hidden
+        tbDone.Visibility = Visibility.Hidden
         Close()
     End Sub
 
-    Private Sub ExitAGNES(sender As Object, e As MouseButtonEventArgs) Handles btnExit.MouseDown
-        'TODO: ADD APPLICATION STYLE MESSAGEBOX
-        Dim yn As MsgBoxResult = MsgBox("Close WCR?", vbYesNo)
-        If yn = vbYes Then
-            WCRModule.UserClosed = True
-            Close()
-        End If
+    Private Sub HoverOver(sender As TextBlock, e As MouseEventArgs) Handles tbViewWCR.MouseEnter, tbCreateWCR.MouseEnter, tbAnother.MouseEnter, tbDone.MouseEnter, tbLoadTenders.MouseEnter, tbNo.MouseEnter, tbYes.MouseEnter
+        sender.Foreground = New SolidColorBrush(Colors.Blue)
+        sender.Effect = HoverDrop
+    End Sub
+
+    Private Sub HoverLeave(sender As TextBlock, e As MouseEventArgs) Handles tbViewWCR.MouseLeave, tbCreateWCR.MouseLeave, tbAnother.MouseLeave, tbDone.MouseLeave, tbLoadTenders.MouseLeave, tbNo.MouseLeave, tbYes.MouseLeave
+        sender.Foreground = New SolidColorBrush(Colors.Black)
+        sender.Effect = LeaveDrop
     End Sub
 
     Public Sub PrintVendorTotalTendersToScreen(ByRef v As VendorObject, bf As Boolean)
@@ -62,22 +68,31 @@
         If bf = True Then
             msgstring = "Unable to read data from the selected file."
             tbHello.Text = msgstring
-            btnLoadTenders.Visibility = Visibility.Hidden
-            btnYes.Visibility = Visibility.Hidden
-            btnNo.Visibility = Visibility.Hidden
-            btnAnother.Visibility = Visibility.Visible
-            btnDone.Visibility = Visibility.Visible
+            tbLoadTenders.Visibility = Visibility.Hidden
+            tbYes.Visibility = Visibility.Hidden
+            tbNo.Visibility = Visibility.Hidden
+            tbAnother.Visibility = Visibility.Visible
+            tbDone.Visibility = Visibility.Visible
         Else
             For Each t In v.Tenders
                 ttl += t.TenderAmt
             Next
             msgstring = "It looks like " & v.VendorName & "" & " has a total of " & FormatCurrency(ttl, 2) & ".  Is this correct?"
             tbHello.Text = msgstring
-            btnLoadTenders.Visibility = Visibility.Hidden
-            btnYes.Visibility = Visibility.Visible
-            btnNo.Visibility = Visibility.Visible
-            btnAnother.Visibility = Visibility.Hidden
-            btnDone.Visibility = Visibility.Hidden
+            tbLoadTenders.Visibility = Visibility.Hidden
+            tbYes.Visibility = Visibility.Visible
+            tbNo.Visibility = Visibility.Visible
+            tbAnother.Visibility = Visibility.Hidden
+            tbDone.Visibility = Visibility.Hidden
+        End If
+    End Sub
+
+    Private Sub ExitAGNES(sender As Object, e As MouseButtonEventArgs) Handles btnExit.MouseDown
+        'TODO: ADD APPLICATION STYLE MESSAGEBOX
+        Dim yn As MsgBoxResult = MsgBox("Close WCR?", vbYesNo)
+        If yn = vbYes Then
+            WCRModule.UserClosed = True
+            Close()
         End If
     End Sub
 End Class
