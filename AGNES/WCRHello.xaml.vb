dbe253443db1f34e391e260e2f68d54283f0dd1a
Brian Freeman <v-brif@microsoft.com>
2018-08-08 12:30:57 -0700

    WCR complete except for design

--- AGNES/WCRHello.xaml.vb
+++ AGNES/WCRHello.xaml.vb
@@ -5,7 +5,8 @@
 
     Private Sub WCRHello_Loaded(sender As Object, e As RoutedEventArgs) Handles Me.Loaded
         'TODO: Map to user table to get short name, etc.
-        ActiveWCR.Author = MySettings.Default.UserName
+        ActiveWCR.Author = "Brian test"
+        ' ActiveWCR.Author = MySettings.Default.UserName
         ActiveWCR.ShortName = "pal"
         tbHello.Text = "Hi, " & ActiveWCR.ShortName & "!  It's me, Agnes, and I'll be guiding you through your WCR entry today." & Chr(13) & Chr(13) & "Let's get started by choosing whether we want to look at a past WCR or create a new one!"
         tbViewWCR.Visibility = Visibility.Visible
@@ -32,59 +33,63 @@
     End Sub
 
     Private Sub LoadTenderFile(sender As Object, e As MouseEventArgs) Handles tbLoadTenders.MouseDown, tbAnother.MouseDown
-        WCRModule.WCR.LoadTenders(Me)
-    End Sub
-
-    Private Sub LoadAnotherTenderFile(sender As Object, e As MouseEventArgs) Handles tbYes.MouseDown
-        tbYes.Visibility = Visibility.Hidden
-        tbNo.Visibility = Visibility.Hidden
-        tbAnother.Visibility = Visibility.Visible
-        tbDone.Visibility = Visibility.Visible
+        WCRModule.WCR.LoadTenders(Me, tbHello)
     End Sub
 
     Private Sub btnDone_Click(sender As Object, e As MouseEventArgs) Handles tbDone.MouseDown
         tbHello.FontSize = 30
         tbLoadTenders.Visibility = Visibility.Hidden
-        tbYes.Visibility = Visibility.Hidden
-        tbNo.Visibility = Visibility.Hidden
         tbAnother.Visibility = Visibility.Hidden
         tbDone.Visibility = Visibility.Hidden
         Close()
     End Sub
 
-    Private Sub HoverOver(sender As TextBlock, e As MouseEventArgs) Handles tbViewWCR.MouseEnter, tbCreateWCR.MouseEnter, tbAnother.MouseEnter, tbDone.MouseEnter, tbLoadTenders.MouseEnter, tbNo.MouseEnter, tbYes.MouseEnter
+    Private Sub HoverOver(sender As TextBlock, e As MouseEventArgs) Handles tbViewWCR.MouseEnter, tbCreateWCR.MouseEnter, tbAnother.MouseEnter, tbDone.MouseEnter, tbLoadTenders.MouseEnter
         sender.Foreground = New SolidColorBrush(Colors.Blue)
         sender.Effect = HoverDrop
     End Sub
 
-    Private Sub HoverLeave(sender As TextBlock, e As MouseEventArgs) Handles tbViewWCR.MouseLeave, tbCreateWCR.MouseLeave, tbAnother.MouseLeave, tbDone.MouseLeave, tbLoadTenders.MouseLeave, tbNo.MouseLeave, tbYes.MouseLeave
+    Private Sub HoverLeave(sender As TextBlock, e As MouseEventArgs) Handles tbViewWCR.MouseLeave, tbCreateWCR.MouseLeave, tbAnother.MouseLeave, tbDone.MouseLeave, tbLoadTenders.MouseLeave
         sender.Foreground = New SolidColorBrush(Colors.Black)
         sender.Effect = LeaveDrop
     End Sub
 
-    Public Sub PrintVendorTotalTendersToScreen(ByRef v As VendorObject, bf As Boolean)
-        Dim t As Tender, ttl As Double, msgstring As String = ""
-        tbHello.FontSize = 24
-        If bf = True Then
-            msgstring = "Unable to read data from the selected file."
-            tbHello.Text = msgstring
-            tbLoadTenders.Visibility = Visibility.Hidden
-            tbYes.Visibility = Visibility.Hidden
-            tbNo.Visibility = Visibility.Hidden
-            tbAnother.Visibility = Visibility.Visible
-            tbDone.Visibility = Visibility.Visible
+    'Public Sub PrintVendorTotalTendersToScreen(ByRef v As VendorObject, bf As Boolean)
+    '//--------DEPRECATED 8/8/18 IN LIEU OF HANDLING MULTIPLE FILES AND CONFIRMATIONS IN THE WCR OBJECT
+    '    Dim t As Tender, ttl As Double, msgstring As String = ""
+    '    tbHello.FontSize = 24
+    '    If bf = True Then
+    '        msgstring = "Unable to read data from the selected file."
+    '        tbHello.Text = msgstring
+    '        tbLoadTenders.Visibility = Visibility.Hidden
+    '        tbYes.Visibility = Visibility.Hidden
+    '        tbNo.Visibility = Visibility.Hidden
+    '        tbAnother.Visibility = Visibility.Visible
+    '        tbDone.Visibility = Visibility.Visible
+    '    Else
+    '        For Each t In v.Tenders
+    '            ttl += t.TenderAmt
+    '        Next
+    '        msgstring = "It looks like " & v.VendorName & "" & " has a total of " & FormatCurrency(ttl, 2) & ".  Is this correct?"
+    '        tbHello.Text = msgstring
+    '        tbLoadTenders.Visibility = Visibility.Hidden
+    '        tbYes.Visibility = Visibility.Visible
+    '        tbNo.Visibility = Visibility.Visible
+    '        tbAnother.Visibility = Visibility.Hidden
+    '        tbDone.Visibility = Visibility.Hidden
+    '    End If
+    'End Sub
+
+    Public Sub TenderLoadComplete(totalfiles, badfiles)
+        If badfiles = 0 Then
+            tbHello.Text = "All selected tenders have been loaded"
         Else
-            For Each t In v.Tenders
-                ttl += t.TenderAmt
-            Next
-            msgstring = "It looks like " & v.VendorName & "" & " has a total of " & FormatCurrency(ttl, 2) & ".  Is this correct?"
-            tbHello.Text = msgstring
-            tbLoadTenders.Visibility = Visibility.Hidden
-            tbYes.Visibility = Visibility.Visible
-            tbNo.Visibility = Visibility.Visible
-            tbAnother.Visibility = Visibility.Hidden
-            tbDone.Visibility = Visibility.Hidden
+            tbHello.Text = (totalfiles - badfiles) & " of the " & totalfiles & " tenders you selected have been loaded."
         End If
+
+        tbLoadTenders.Visibility = Visibility.Hidden
+        tbAnother.Visibility = Visibility.Visible
+        tbDone.Visibility = Visibility.Visible
     End Sub
 
     Private Sub ExitAGNES(sender As Object, e As MouseButtonEventArgs) Handles btnExit.MouseDown
