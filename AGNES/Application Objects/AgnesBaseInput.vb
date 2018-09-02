a5b2cc7a90a59716203e224b6d4c46ff34d6f937
Brian Freeman <v-brif@microsoft.com>
2018-09-02 16:47:13 -0700

    Currency box built on top of base input textbox

--- AGNES/Application Objects/AgnesBaseInput.vb
+++ AGNES/Application Objects/AgnesBaseInput.vb
@@ -4,6 +4,13 @@
         Show
         Hide
     End Enum
+
+    Public Enum FontSz
+        Small
+        Standard
+        Large
+        VeryLarge
+    End Enum
     Private _flare As Boolean
     Public Property Flare As Boolean
         Get
@@ -22,25 +29,58 @@
         End Set
     End Property
 
-    Public Sub New(ByVal v As VerticalAlignment, ByVal h As HorizontalAlignment, Optional ByVal s As String = "", Optional ByVal tw As TextWrapping = TextWrapping.Wrap)
-
-        With Me
-            .HorizontalAlignment = Windows.HorizontalAlignment.Left
-            .Height = 32
-            .Width = 128
-            .VerticalAlignment = VerticalAlignment.Top
-        End With
+    Public Sub New(FieldWidth As Integer, ByVal v As VerticalAlignment, ByVal h As HorizontalAlignment, FontSize As FontSz, Optional ta As TextAlignment = TextAlignment.Left, Optional ByVal s As String = "", Optional ByVal tw As TextWrapping = TextWrapping.Wrap)
+        HorizontalAlignment = h
+        VerticalAlignment = v
+        Width = FieldWidth
         Dim errorflare As New Effects.BlurEffect With {.KernelType = Effects.KernelType.Gaussian, .Radius = 10,
             .RenderingBias = Effects.RenderingBias.Performance}
         Dim myrct As New Rectangle With {.Name = "ErrorRectangle", .Fill = Brushes.Red, .Opacity = 0.33, .StrokeThickness = 1,
             .Effect = errorflare, .Visibility = Visibility.Hidden}
-        Dim mytxt As New TextBox With {.Name = "TextBox", .Height = 24, .Width = 120, .Margin = New Thickness(4, 4, 0, 0),
-            .BorderBrush = Brushes.LightGray, .Text = s, .HorizontalAlignment = h, .VerticalAlignment = v, .TextWrapping = tw}
+
+        Dim mytxt As New TextBox With {.Name = "TextBox", .BorderBrush = Brushes.LightGray, .Text = s, .TextAlignment = ta,
+            .TextWrapping = tw, .HorizontalAlignment = h, .VerticalAlignment = v}
+
+        Select Case FontSize
+            Case FontSz.Small
+                Height = 24
+                With mytxt
+                    .Height = 16
+                    .Width = FieldWidth - 8
+                    .Margin = New Thickness(4, 4, 0, 0)
+                    .FontSize = 8
+                End With
+            Case FontSz.Standard
+                Height = 28
+                With mytxt
+                    .Height = 20
+                    .Width = FieldWidth - 8
+                    .Margin = New Thickness(4, 4, 0, 0)
+                    .FontSize = 12
+                End With
+            Case FontSz.Large
+                Height = 36
+                With mytxt
+                    .Height = 28
+                    .Width = FieldWidth - 8
+                    .Margin = New Thickness(4, 4, 0, 0)
+                    .FontSize = 18
+                End With
+            Case FontSz.VeryLarge
+                Height = 44
+                With mytxt
+                    .Height = 36
+                    .Width = FieldWidth - 8
+                    .Margin = New Thickness(4, 4, 0, 0)
+                    .FontSize = 24
+                End With
+        End Select
 
         With Children
             .Add(myrct)
             .Add(mytxt)
         End With
     End Sub
+
 
 End Class
--- AGNES/Xaml Modules/HR Audit/HRAudit.xaml.vb
+++ AGNES/Xaml Modules/HR Audit/HRAudit.xaml.vb
@@ -1,14 +1,13 @@
 ﻿Public Class HRAudit
     Private Sub HRAudit_Loaded(sender As Object, e As RoutedEventArgs) Handles Me.Loaded
-        Dim testme As New AgnesBaseInput(VerticalAlignment.Top, HorizontalAlignment.Left, "Hey") With {.Name = "TestFlare"}
+        Dim testme As New CurrencyBox(204, True, True, False, False, True, AgnesBaseInput.FontSz.VeryLarge)
         grdMain.Children.Add(testme)
-
     End Sub
 
     Private Sub Button_Click(sender As Object, e As RoutedEventArgs)
         For Each a As Object In grdMain.Children
-            If TypeOf (a) Is AgnesBaseInput Then
-                Dim b As AgnesBaseInput = a
+            If TypeOf (a) Is CurrencyBox Then
+                Dim b As CurrencyBox = a
                 b.Flare = Not b.Flare
             End If
         Next
