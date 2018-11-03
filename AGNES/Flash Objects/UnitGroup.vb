0637003499521badcdd287139ca5c1b6246bc9a5
Brian Freeman <v-brif@microsoft.com>
2018-11-02 18:51:39 -0700

    Templating and light testing complete for Cafes, Field, and A/V forecast module

--- AGNES/Flash Objects/UnitGroup.vb
+++
@@ -1,7 +0,0 @@
-﻿Public Class UnitGroup
-    '// Container class for UnitFlash class objects, used to construct the Flash interface itself, specifically informing the 
-    '   UnitChooser control
-
-    Public Property UnitGroupName As String
-    Public Property UnitsInGroup As New List(Of UnitFlash)
-End Class
