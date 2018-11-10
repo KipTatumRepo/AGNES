b341f8f28e4f7e4b27c18757d270f2d0ecea3ca8
Brian Freeman <v-brif@microsoft.com>
2018-11-10 08:37:17 -0800

    Loads of XAML design alignments between windows + framing out the Vendor Manager

---
+++ AGNES/XAML Components/Vendor Management/VendorSchedule.xaml.vb
@@ -0,0 +1,32 @@
+﻿Public Class VendorSchedule
+
+#Region "Properties"
+    Public Property MSP As PeriodChooser
+    Public Property Wk As WeekChooser
+#End Region
+
+#Region "Constructor"
+    Public Sub New()
+        InitializeComponent()
+        Dim currmsp As Byte = GetCurrentPeriod(FormatDateTime(Now(), DateFormat.ShortDate))
+        Dim currwk As Byte = GetCurrentWeek(FormatDateTime(Now(), DateFormat.ShortDate))
+        Wk = New WeekChooser(1, GetMaxWeeks(currmsp), currwk)
+        MSP = New PeriodChooser(Wk, 1, 12, currmsp)
+        MSP.DisableSelectAll = False
+        Dim sep As New Separator
+        With tlbVendors.Items
+            .Add(MSP)
+            .Add(sep)
+            .Add(Wk)
+        End With
+    End Sub
+#End Region
+
+#Region "Public Methods"
+
+#End Region
+
+#Region "Private Methods"
+
+#End Region
+End Class
