fa4aaccf19568e3229794d5cb9956cc4485707fb
Brian Freeman <v-brif@microsoft.com>
2018-11-11 09:18:41 -0800

    Base routine for week schedule create framed out

--- AGNES/Vendor Objects/ScheduleWeek.vb
+++ AGNES/Vendor Objects/ScheduleWeek.vb
@@ -1,4 +1,34 @@
 ﻿Imports System.ComponentModel
 Public Class ScheduleWeek
+    Inherits DockPanel
+#Region "Properties"
 
+#End Region
+
+#Region "Constructor"
+
+#End Region
+
+#Region "Public Methods"
+    Public Sub Update(p As Byte, w As Byte)
+        Children.Clear()
+        Dim qwd = From f As Dates In SharedDataGroup.Dates
+                  Where f.MS_FY = CurrentFiscalYear And
+                      f.MS_Period = p And
+                      f.Week = w
+
+        For Each f In qwd
+            If Weekday(f.Date_ID, FirstDayOfWeek.Monday) < 6 Then CreateWeekDay(f.Date_ID, f.IS_WEEKEND_HOLIDAY)
+        Next
+
+    End Sub
+#End Region
+
+#Region "Private Methods"
+    Private Sub CreateWeekDay(dt, hol)
+        Dim newday As New TextBlock With {.Text = FormatDateTime(dt, DateFormat.ShortDate)}
+        If hol = 1 Then newday.IsEnabled = False
+        Children.Add(newday)
+    End Sub
+#End Region
 End Class
--- AGNES/XAML Components/Vendor Management/VendorSchedule.xaml.vb
+++ AGNES/XAML Components/Vendor Management/VendorSchedule.xaml.vb
@@ -8,9 +8,11 @@
 #Region "Constructor"
     Public Sub New()
         InitializeComponent()
+        '// Add period and week slicers
         Dim currmsp As Byte = GetCurrentPeriod(FormatDateTime(Now(), DateFormat.ShortDate))
         Dim currwk As Byte = GetCurrentWeek(FormatDateTime(Now(), DateFormat.ShortDate))
         Wk = New WeekChooser(1, GetMaxWeeks(currmsp), currwk)
+        AddHandler Wk.PropertyChanged, AddressOf WeekChanged
         MSP = New PeriodChooser(Wk, 1, 12, currmsp)
         MSP.DisableSelectAll = False
         Dim sep As New Separator
@@ -19,7 +21,13 @@
             .Add(sep)
             .Add(Wk)
         End With
-        PopulateVendors()
+
+        '// Add week object, with days, locations, and data load being subfunctions
+        Dim wkSched As New ScheduleWeek
+        wkSched.Update(MSP.CurrentPeriod, Wk.CurrentWeek)
+        grdWeek.Children.Add(wkSched)
+
+        PopulateVendors() '//   Any consideration of day-to-day vendor availability as to whether to show them?
     End Sub
 
 #End Region
@@ -37,5 +45,12 @@
             stkVendors.Children.Add(nv)
         Next
     End Sub
+#End Region
+
+#Region "Event Listeners"
+    Private Sub WeekChanged()
+
+    End Sub
+
 #End Region
 End Class
