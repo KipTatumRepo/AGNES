d1813da2673c32c9ca0f64e8a1368c12729fe462
Brian Freeman <v-brif@microsoft.com>
2018-11-25 07:48:05 -0800

    Corrected issue with displaying statuses

--- AGNES/My Project/Resources.Designer.vb
+++ AGNES/My Project/Resources.Designer.vb
@@ -303,6 +303,16 @@ Namespace My.Resources
         '''<summary>
         '''  Looks up a localized resource of type System.Drawing.Bitmap.
         '''</summary>
+        Friend ReadOnly Property HRMgr() As System.Drawing.Bitmap
+            Get
+                Dim obj As Object = ResourceManager.GetObject("HRMgr", resourceCulture)
+                Return CType(obj,System.Drawing.Bitmap)
+            End Get
+        End Property
+        
+        '''<summary>
+        '''  Looks up a localized resource of type System.Drawing.Bitmap.
+        '''</summary>
         Friend ReadOnly Property Journal() As System.Drawing.Bitmap
             Get
                 Dim obj As Object = ResourceManager.GetObject("Journal", resourceCulture)
