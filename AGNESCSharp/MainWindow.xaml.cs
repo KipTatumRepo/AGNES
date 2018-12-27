ba1a1dc6387c290e65c92932c919f024a6d4c265
Kip Tatum <v-datatu@microsoft.com>
2018-12-27 09:06:03 -0800

    HR module Code added

--- AGNESCSharp/MainWindow.xaml.cs
+++ AGNESCSharp/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AGNESCSharp.Entity_Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -27,6 +28,8 @@ namespace AGNESCSharp
         public MainWindow()
         {
             InitializeComponent();
+            HRMgr GoTo = new HRMgr();
+            GoTo.ShowDialog();
         }
     }
 }
