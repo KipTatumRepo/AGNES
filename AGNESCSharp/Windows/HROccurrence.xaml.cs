964e216fe7896666a71acb89b16d13878ce4c2c8
Kip Tatum <v-datatu@microsoft.com>
2018-12-28 08:57:02 -0800

    erase image added

--- AGNESCSharp/Windows/HROccurrence.xaml.cs
+++ AGNESCSharp/Windows/HROccurrence.xaml.cs
@@ -472,8 +472,20 @@ namespace AGNESCSharp
 
         private void SaveImage_MouseLeave(object sender, MouseEventArgs e)
         {
-            //saveImage.Width = 58;
-            //saveImage.Height = 50;
+            saveImage.Width = 58;
+            saveImage.Height = 50;
+        }
+
+        private void EraseImage_MouseEnter(object sender, MouseEventArgs e)
+        {
+            eraseImage.Width = 66;
+            eraseImage.Height = 58;
+        }
+
+        private void EraseImage_MouseLeave(object sender, MouseEventArgs e)
+        {
+            eraseImage.Width = 58;
+            eraseImage.Height = 50;
         }
     }
 }
