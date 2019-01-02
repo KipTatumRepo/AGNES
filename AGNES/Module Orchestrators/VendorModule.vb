9bda9c558d4a24f87b5f856fc175082ab2743cd8
Brian Freeman <v-brif@microsoft.com>
2019-01-02 13:28:54 -0800

    Finalized print jobs for Vendor Scheduler - ready for beta

--- AGNES/Module Orchestrators/VendorModule.vb
+++ AGNES/Module Orchestrators/VendorModule.vb
@@ -11,6 +11,17 @@
         VendorSched.ShowDialog()
     End Sub
 
+    Public Function GetAlternateStationName(bn, sn) As String
+        'CRITICAL: ALTERNATE STATION NAMES HARD CODED FOR TESTING - WILL NEED TO CONVERT TO DB TABLE FOR EDITOR/MAINTENANCE
+        Select Case bn
+            Case "Advanta"
+                If sn = 3 Then Return "NP"
+            Case "Café 31"
+                If sn = 2 Then Return "POD"
+        End Select
+        Return "Station " & sn
+    End Function
+
 #End Region
 
 #Region "Private Methods"
