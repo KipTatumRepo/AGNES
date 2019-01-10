b941f0f85b5c3937981845c7ae35a2e958f11067
Brian Freeman <v-brif@microsoft.com>
2019-01-10 08:29:51 -0800

    Added error traps for initialization of HR Mgr

--- AGNES/Module Orchestrators/CSharpModules.vb
+++ AGNES/Module Orchestrators/CSharpModules.vb
@@ -1,13 +1,28 @@
 ﻿Module CSharpModules
     Public Sub RunHRMgrModule()
+        'CRITICAL: ERROR TRAP TEMPORARILY ADDED
         Dim UserAccess As Long
-        If My.Settings.UserLevel <> 4 Then
-            UserAccess = 0
-        Else
-            UserAccess = My.Settings.UserID
-        End If
-        Dim HRMgr As New AGNESCSharp.MainWindow(UserAccess)
-        HRMgr.Close()
+        Try
+            If My.Settings.UserLevel <> 4 Then
+                UserAccess = 0
+            Else
+                UserAccess = My.Settings.UserID
+            End If
+            MsgBox("First initializing action okay for userID: " & UserAccess)
+        Catch ex As Exception
+            MsgBox("First initializing action failed - error was " & ex.Message)
+        End Try
+
+
+
+        Try
+            Dim HRMgr As New AGNESCSharp.MainWindow(UserAccess)
+            HRMgr.Close()
+        Catch ex As Exception
+            MsgBox("Second initializing action failed - error was " & ex.Message)
+        End Try
+
+
     End Sub
 
 End Module
